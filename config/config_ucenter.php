b06ee8989e8d052b998c39aeee39d8062db496c4
latticet <latticet@163.com>
2016-03-10 14:05:51 +0800

    first commit

---
+++ config/config_ucenter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', 'root');
+define('UC_DBNAME', 'jjl');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`jjl`.jjl_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'utf-8');
+define('UC_KEY', 'Q9d3v2y6z1C5bdI61440s2mde5B5acu1u7b8e8o0ff669eLbN8f0u9fbfeT75eef');
+define('UC_API', 'http://124.251.43.203/jjl/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
